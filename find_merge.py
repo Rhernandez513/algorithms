95d74096d558337b1c501a22bb75856429c0ea0f
Robert David Hernandez <Rhernandez513@users.noreply.github.com>
2017-11-16 15:07:40 -0600

    Update find_merge.py

--- find_merge.py
+++ find_merge.py
@@ -13,39 +13,59 @@ def read_input(file_name):
     return content
 
 
+# http://www.geeksforgeeks.org/union-find/
+# A utility function to find the subset of an element i
+def find_parent(self, parent, i):
+    if parent[i] == -1:
+        return i
+    if parent[i] != -1:
+        return self.find_parent(parent, parent[i])
 
-def find (value, disjoint_set):
-    disjoint_set_list = list(disjoint_set)
-    return disjoint_set_list.index(value)
+# http://www.geeksforgeeks.org/union-find/
+# A utility function to do union of two subsets
+def union(self,parent,x,y):
+    x_set = self.find_parent(parent, x)
+    y_set = self.find_parent(parent, y)
+    parent[x_set] = y_set
 
-
-def merge (hashSet, disjoint_set, value_one, value_two):
-
-    # PSEUDO
-
-    # wList = a.find(two)
-    res  = list(find(value_two, disjoint_set))
-
-    working_list = hashSet(res)
-    for item in working_list:
-        disjoint_set[item] = value_one
-        working_list[item].append(value_two)
-    del hashSet[value_two]
-
+def connectivity(x, y):
+    return 0
 
 
 if __name__ == "__main__":
     file_location = "/home/robert/PycharmProjects/find/Graph.txt"
+    file_location = "/home/robert/PycharmProjects/find/dummy.txt"
     file_contents = read_input(file_location)
 
-    hash_set = dict("abcdefg")
-    for item in hash_set:
-        hash_set[item][item] = dict()
+    # should now containt a list of all the graph edges
+    # with each entry looking like
+    # 11111, 222222
+    graph_edge_list = file_contents.split('\n')
+
+    # Remove last element
+
+    del graph_edge_list[len(graph_edge_list) - 1]
+
+    total_set = set()
+
+    for item in graph_edge_list:
+        result_array = item.split(',')
+        total_set.add(result_array[0])
+        total_set.add(result_array[1])
+
+    print(total_set)
+    dict_pairs = dict()
+
+    for item in total_set:
+        dict_pairs[item] = item
+
+    print(dict_pairs)
+    # at this point the graph should be in a list, prepared for union merge
 
 
-    disjoint_set = dict("hijklmn")
     value_one = "b"
     value_two = "a"
+
     value_index = find(value_one, hash_set)
 
     merge(hash_set, disjoint_set, value_one, value_two)
