98e5ccfbd1b4513d5028b33a08c746107b16bd48
Robert David Hernandez <Rhernandez513@users.noreply.github.com>
2017-11-23 16:20:22 -0600

    Update simpleapp.scala

--- simpleapp.scala
+++ simpleapp.scala
@@ -2,20 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-<!-- This Source Code Form is subject to the terms of the Mozilla Public
-   - License, v. 2.0. If a copy of the MPL was not distributed with this
-   - file, You can obtain one at http://mozilla.org/MPL/2.0/. -->
-
-This Source Code Form is subject to the terms of the Mozilla Public
-License, v. 2.0. If a copy of the MPL was not distributed with this
-file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-
-
 /* SimpleApp.scala */
 import org.apache.spark.sql.SparkSession
 
